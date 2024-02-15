df6addd62d3ce9d300a093504867f1c583df3778
Minh Luan <donguyenminhluan96@gmail.com>
2024-02-15 23:47:58 +0700

    chore: fix bugs modal and update message

--- src/app/hooks/useSendWishesModal/useSendWishesModal.tsx
+++ src/app/hooks/useSendWishesModal/useSendWishesModal.tsx
@@ -1,10 +1,21 @@
-import {DialogContent, DialogTitle, Grid, Modal, ModalDialog, Textarea, Button} from "@mui/joy";
+import {
+    DialogContent,
+    DialogTitle,
+    Grid,
+    Modal,
+    ModalDialog,
+    Textarea,
+    Button,
+    ModalOverflow,
+    ModalClose
+} from "@mui/joy";
 import React, {useCallback} from "react";
 import styled from 'styled-components';
 import {Transition} from "react-transition-group";
 import {useGetUserFromQueryParams} from "@/app/hooks/useGetUserFromQueryParams/useGetUserFromQueryParams";
 import {useSubmitResponse} from "@/app/hooks/useSubmitResponse/useSubmitResponse";
 import {useGoogleReCaptcha} from "react-google-recaptcha-v3";
+import {useTranslations} from "next-intl";
 
 type UseSendWishesModalOutput = {
     modal: React.ReactElement;
@@ -14,12 +25,14 @@ type UseSendWishesModalOutput = {
 type UseSendWishesModalProps = {
     guestUser?: string;
 }
-export function useSendWishesModal({ guestUser }: UseSendWishesModalProps): UseSendWishesModalOutput {
-    const { executeRecaptcha } = useGoogleReCaptcha();
+
+export function useSendWishesModal({guestUser}: UseSendWishesModalProps): UseSendWishesModalOutput {
+    const {executeRecaptcha} = useGoogleReCaptcha();
+    const t = useTranslations('RejectModal');
     
     const [open, setOpen] = React.useState<boolean>(false);
-    const { user } = useGetUserFromQueryParams();
-    const { submit } = useSubmitResponse();
+    const {user} = useGetUserFromQueryParams();
+    const {submit} = useSubmitResponse();
     
     const onSubmitClick = useCallback(async (data: string) => {
         if (!executeRecaptcha) return;
@@ -28,7 +41,7 @@ export function useSendWishesModal({ guestUser }: UseSendWishesModalProps): UseS
             from: guestUser,
             blessings: data
         };
-
+        
         await submit({
             token,
             userId: user?.id,
@@ -53,8 +66,8 @@ export function useSendWishesModal({ guestUser }: UseSendWishesModalProps): UseS
                                 backdropFilter: 'none',
                                 transition: `opacity 400ms, backdrop-filter 400ms`,
                                 ...{
-                                    entering: { opacity: 1, backdropFilter: 'blur(8px)' },
-                                    entered: { opacity: 1, backdropFilter: 'blur(8px)' },
+                                    entering: {opacity: 1, backdropFilter: 'blur(8px)'},
+                                    entered: {opacity: 1, backdropFilter: 'blur(8px)'},
                                 }[state],
                             },
                         },
@@ -63,70 +76,70 @@ export function useSendWishesModal({ guestUser }: UseSendWishesModalProps): UseS
                         visibility: state === 'exited' ? 'hidden' : 'visible',
                     }}
                 >
-                    <StyledModalDialog
-                        sx={{
-                            opacity: 0,
-                            transition: `opacity 300ms`,
-                            ...{
-                                entering: { opacity: 1 },
-                                entered: { opacity: 1 },
-                            }[state],
-                        }}
-                    >
-                        <Grid container spacing={2} justifyContent='center'>
-                            <Grid xs={6}>
-                                <StyledDialogTitle>
-                                    Send wishes
-                                </StyledDialogTitle>
-                            </Grid>
-                            
-                            <Grid xs={12}>
-                                <StyledDialogContent>
-                                    <p>While we deeply regret that some of you won’t be able to join us on our special
-                                        day, we appreciate your warm wishes and support. If you’d like to leave a
-                                        message or share your blessings, please feel free to do so below. Your kind
-                                        words mean the world to us</p>
-                                </StyledDialogContent>
-                            </Grid>
-                            
-                            <Grid xs={12}>
-                                <form
-                                    onSubmit={async (event) => {
-                                        event.preventDefault();
-                                        const formData = new FormData(event.currentTarget);
-                                        const formJson = Object.fromEntries((formData as any).entries());
-                                        await onSubmitClick(formJson?.['form']);
-                                    }}
-                                >
-                                    <StyledFormContent>
-                                        <Textarea
-                                            name='form'
-                                            placeholder="Share your blessings here"
-                                            required
-                                            variant='soft'
-                                            sx={{ mb: 10 }}
-                                            minRows={10}
-                                            maxRows={14}
-                                            slotProps={{ textarea: { maxLength: 2000 } }}
-                                        />
-                                        <Button
-                                            size='lg'
-                                            color='warning'
-                                            variant='soft'
-                                            type='submit'
-                                        >
-                                            Send
-                                        </Button>
-                                    </StyledFormContent>
+                    <ModalOverflow>
+                        <StyledModalDialog
+                            sx={{
+                                opacity: 0,
+                                transition: `opacity 300ms`,
+                                ...{
+                                    entering: {opacity: 1},
+                                    entered: {opacity: 1},
+                                }[state],
+                            }}
+                        >
+                            <ModalClose size='lg' />
+                            <Grid container spacing={2} justifyContent='center'>
+                                <Grid xs={6}>
+                                    <StyledDialogTitle>
+                                        {t("title")}
+                                    </StyledDialogTitle>
+                                </Grid>
+                                
+                                <Grid xs={12}>
+                                    <StyledDialogContent>
+                                        <p>{t("subTitle")}</p>
+                                    </StyledDialogContent>
+                                </Grid>
+                                
+                                <Grid xs={12}>
+                                    <form
+                                        onSubmit={async (event) => {
+                                            event.preventDefault();
+                                            const formData = new FormData(event.currentTarget);
+                                            const formJson = Object.fromEntries((formData as any).entries());
+                                            await onSubmitClick(formJson?.['form']);
+                                        }}
+                                    >
+                                        <StyledFormContent>
+                                            <Textarea
+                                                name='form'
+                                                placeholder={t("placeholder")}
+                                                required
+                                                variant='soft'
+                                                sx={{mb: 10}}
+                                                minRows={10}
+                                                maxRows={14}
+                                                slotProps={{textarea: {maxLength: 2000}}}
+                                            />
+                                            <Button
+                                                size='lg'
+                                                color='warning'
+                                                variant='soft'
+                                                type='submit'
+                                            >
+                                                {t("send")}
+                                            </Button>
+                                        </StyledFormContent>
                                     
-                                </form>
+                                    </form>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    
-                    
-                    </StyledModalDialog>
+                        
+                        
+                        </StyledModalDialog>
+                    </ModalOverflow>
                 </Modal>
-                )}
+            )}
         </Transition>
     );
     
@@ -146,25 +159,25 @@ const StyledDialogTitle = styled(DialogTitle)`
 const StyledDialogContent = styled(DialogContent)`
     text-align: center;
     justify-content: center;
-    
+
     font-size: 16px;
 `;
 
 const StyledModalDialog = styled(ModalDialog)`
-    padding: 4rem 6rem !important;
+    padding: 4rem 4rem !important;
 `;
 
 const StyledFormContent = styled.div`
-    
+
     textarea {
         padding: 16px;
         font-size: 16px !important;
     }
-    
+
     button {
         min-height: 4rem;
         font-size: 16px;
         width: 100%;
     }
-    
+
 `;
